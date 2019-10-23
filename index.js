641c04055d4878116b1de8d68d77ad10fa4d3295
Sundar Maharjan <sundarmaharjan43@gmail.com>
2019-10-23 17:30:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// add solution here
+function theBeatlesPlay(musicians, instrument) {
+  var array = []
+  for ( var i = 0; 1 < musicians.length; i++) {
+    array.push(musicians[i] + ' plays ' + instrument[i]); 
+    return array;
+  }
+}
+	
